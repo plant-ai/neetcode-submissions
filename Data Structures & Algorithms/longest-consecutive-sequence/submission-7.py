97220f1c1faa2089033a1ae84ae138a800c5ece6
plant-ai <149395424+plant-ai@users.noreply.github.com>
2026-07-05 22:04:00 +0100

    Add: longest-consecutive-sequence - submission-7

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-7.py
@@ -0,0 +1,22 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        numbers = {}
+        longest = 1
+        for num in nums:
+            if num in numbers:
+                numbers[num] += 1
+                continue
+            numbers[num] = 1
+        for key in numbers:
+            if (key - 1) in numbers:
+                continue
+            cur_longest = 1
+            nxt = key + 1
+            while nxt in numbers:
+                cur_longest += 1
+                nxt += 1
+            if cur_longest > longest:
+                longest = cur_longest
+        return longest
